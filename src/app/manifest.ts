c9a5220f31802864b3a7ab3b037af07aee75ee1c
maedatatsuya226 <ibiutscsb@gmail.com>
2026-03-06 18:08:25 +0900

    feat: change app icon to new hospital logo

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,14 +11,14 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#f97316',
     icons: [
       {
-        src: '/icon.jpg',
+        src: '/icon.png',
         sizes: '192x192',
-        type: 'image/jpeg',
+        type: 'image/png',
       },
       {
-        src: '/icon.jpg',
+        src: '/icon.png',
         sizes: '512x512',
-        type: 'image/jpeg',
+        type: 'image/png',
       },
     ],
   }
